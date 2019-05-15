4d4cf8f86a45673e01a9ebf7f53507737c7cacc6
LarsHulsmans <33724749+LarsHulsmans@users.noreply.github.com>
2019-05-15 18:19:57 +0200

    setup

---
+++ src/main/java/nl/avans/wordcrex/Users.java
@@ -0,0 +1,38 @@
+package nl.avans.wordcrex;
+import nl.avans.wordcrex.data.Database;
+
+import java.util.ArrayList;
+
+public class Users {
+
+    private Database database;
+
+    private ArrayList<User> userList = new ArrayList<User>();
+
+    public Users(Database d)
+    {
+        this.database = d;
+    }
+
+    public void AddUser(User u)
+    {
+        this.userList.add(u);
+    }
+
+    public void FillUserList()
+    {
+
+    }
+
+}
+class User
+{
+    String username;
+    String[] roles;
+
+    public User(String n, String[] r)
+    {
+        this.username = n;
+        this.roles = r;
+    }
+}
