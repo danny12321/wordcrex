973feebd1f8d5643cc1b0b7fab027fff7dcee0e7
LarsHulsmans <33724749+LarsHulsmans@users.noreply.github.com>
2019-05-21 21:58:12 +0200

    created view and controller couldn't get it to work tho

---
+++ src/main/java/nl/avans/wordcrex/controller/impl/ManagerController.java
@@ -0,0 +1,42 @@
+package nl.avans.wordcrex.controller.impl;
+
+import nl.avans.wordcrex.Main;
+import nl.avans.wordcrex.controller.Controller;
+import nl.avans.wordcrex.model.User;
+import nl.avans.wordcrex.view.View;
+import nl.avans.wordcrex.view.impl.ManagerView;
+
+import java.util.Map;
+import java.util.function.Function;
+
+public class ManagerController extends Controller<User> {
+    public ManagerController(Main main, Function<User, User> fn) {
+        super(main, fn);
+    }
+
+    @Override
+    public View<? extends Controller<User>> createView() {
+        return new ManagerView(this);
+    }
+
+    public Map<String, String> getStatistics() {
+        return Map.of(
+            "Games won", "20",
+            "Games lost", "5",
+            "Games tied", "2",
+            "Games forfeited", "1",
+            "Highest game score", "420",
+            "Highest word score", "woordje (80)",
+            "Highest game bonus", "0",
+            "Total seven letter bonus", "0"
+        );
+    }
+
+    public String getDisplayName() {
+        return this.getModel().getDisplayName();
+    }
+
+    public String getInitial() {
+        return this.getModel().getInitial();
+    }
+}
