921a84148218cfba529ab9cad6215cdef6f64810
LarsHulsmans <lars.hulsmans14@gmail.com>
2019-05-22 10:09:28 +0200

    searchbox added

--- src/main/java/nl/avans/wordcrex/view/impl/ManagerView.java
+++ src/main/java/nl/avans/wordcrex/view/impl/ManagerView.java
@@ -7,6 +7,7 @@ import nl.avans.wordcrex.util.Fonts;
 import nl.avans.wordcrex.util.StringUtil;
 import nl.avans.wordcrex.view.View;
 import nl.avans.wordcrex.widget.Widget;
+import nl.avans.wordcrex.widget.impl.InputWidget;
 import nl.avans.wordcrex.widget.impl.ScrollbarWidget;
 
 import java.awt.*;
@@ -14,9 +15,8 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ManagerView extends View<ManagerController> {
-   // private final ScrollbarWidget scrollbar = new ScrollbarWidget((scroll) -> this.scroll = scroll);
 
-    private int scroll;
+    private String username;
 
     public ManagerView(ManagerController controller) {
         super(controller);
@@ -34,7 +34,7 @@ public class ManagerView extends View<ManagerController> {
     @Override
     public List<Widget> getChildren() {
         return List.of(
-            //this.scrollbar
+            new InputWidget("Name", 0, 25, Main.FRAME_SIZE,50, (value) -> this.username = value)
         );
     }
 }
