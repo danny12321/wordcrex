33253ce7d458ef138c4649b61e0545e100c03608
Lex Houf <26674115+houfio@users.noreply.github.com>
2019-06-08 16:21:21 +0200

    Add missing fields

--- src/main/java/nl/avans/wordcrex/model/Wordcrex.java
+++ src/main/java/nl/avans/wordcrex/model/Wordcrex.java
@@ -1,28 +1,42 @@
-package nl.avans.wordcrex.model2;
+package nl.avans.wordcrex.model;
 
+import nl.avans.wordcrex.data.Database;
 import nl.avans.wordcrex.util.Persistable;
 
-public class Wordcrex implements Persistable {
-    public final User user;
+import java.util.List;
 
-    public Wordcrex(User user) {
+public class Wordcrex implements Persistable {
+    private final Database database;
+
+    public final User user;
+    public final List<Tile> tiles;
+    public final List<Dictionary> dictionaries;
+
+    public Wordcrex(Database database, User user, List<Tile> tiles, List<Dictionary> dictionaries) {
+        this.database = database;
         this.user = user;
+        this.tiles = tiles;
+        this.dictionaries = dictionaries;
+    }
+
+    public static Wordcrex initialize(Database database) {
+        throw new RuntimeException();
     }
 
     @Override
     public Wordcrex persist() {
-        return this;
+        throw new RuntimeException();
     }
 
     public Wordcrex register(String username, String password) {
-        return this.login(username, password);
+        throw new RuntimeException();
     }
 
     public Wordcrex login(String username, String password) {
-        return this;
+        throw new RuntimeException();
     }
 
     public Wordcrex logout() {
-        return this;
+        throw new RuntimeException();
     }
 }
