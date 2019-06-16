90c08f36392925e17d51e7801f7e1358241c5e57
houfio <26674115+houfio@users.noreply.github.com>
2019-06-16 15:55:07 +0200

    Fix score calculation

--- src/main/java/nl/avans/wordcrex/model/TileSide.java
+++ src/main/java/nl/avans/wordcrex/model/TileSide.java
@@ -1,16 +1,24 @@
 package nl.avans.wordcrex.model;
 
+import java.util.stream.Stream;
+
 public enum TileSide {
-    NORTH(0, -1),
-    EAST(1, 0),
-    SOUTH(0, 1),
-    WEST(-1, 0);
+    NORTH(0, -1, TileAxis.VERTICAL),
+    EAST(1, 0, TileAxis.HORIZONTAL),
+    SOUTH(0, 1, TileAxis.HORIZONTAL),
+    WEST(-1, 0, TileAxis.VERTICAL);
 
     public final int x;
     public final int y;
+    public final TileAxis axis;
 
-    TileSide(int x, int y) {
+    TileSide(int x, int y, TileAxis axis) {
         this.x = x;
         this.y = y;
+        this.axis = axis;
+    }
+
+    public static TileSide[] ofAxis(TileAxis axis) {
+        return Stream.of(TileSide.values()).filter((t) -> t.axis == axis).toArray(TileSide[]::new);
     }
 }
